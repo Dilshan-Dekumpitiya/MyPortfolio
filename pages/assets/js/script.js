7b9b41799ae0286da0053a3be4809014869e561b
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-09-28 15:58:31 +0530

    add project pages with media queries

--- pages/assets/js/script.js
+++ pages/assets/js/script.js
@@ -25,32 +25,27 @@ $(document).mousemove(function(event) {
 /* ------------------------ Projects Page -------------------- */
 $('#btnProject1').on('click', function () {
     $("#btnProject1>a").attr("target", "_blank");
-    $("#btnProject1>a").attr("href", "https://github.com/AshanSandeep06/Restaurant_Management-POS_System");
+    $("#btnProject1>a").attr("href", "https://github.com/Dilshan-Dekumpitiya/Production-and-By-Product-Fuel-Management-System-Using-Layerd-Architecture");
 });
 
 $('#btnProject2').on('click', function () {
     $("#btnProject2>a").attr("target", "_blank");
-    $("#btnProject2>a").attr("href", "https://github.com/AshanSandeep06/Hostel_Management_System-Hibernate-CourseWork");
+    $("#btnProject2>a").attr("href", "https://github.com/Dilshan-Dekumpitiya/Hostel-Management-System-Using-Hibernate");
 });
 
 $('#btnProject3').on('click', function () {
     $("#btnProject3>a").attr("target", "_blank");
-    $("#btnProject3>a").attr("href", "https://github.com/AshanSandeep06/Hotel_Reservation-System");
+    $("#btnProject3>a").attr("href", "https://github.com/Dilshan-Dekumpitiya/Group-Chat-App-Using-Java-Socket-Programming");
 });
 
 $('#btnProject4').on('click', function () {
     $("#btnProject4>a").attr("target", "_blank");
-    $("#btnProject4>a").attr("href", "https://github.com/AshanSandeep06/Supermarket_System-Layered_Architecture.git");
+    $("#btnProject4>a").attr("href", "https://github.com/Dilshan-Dekumpitiya/QR-Code-Generator-Using-JavaFX");
 });
 
 $('#btnProject5').on('click', function () {
     $("#btnProject5>a").attr("target", "_blank");
-    $("#btnProject5>a").attr("href", "https://github.com/AshanSandeep06/Vehicle_Parking-System.git");
-});
-
-$('#btnProject6').on('click', function () {
-    $("#btnProject6>a").attr("target", "_blank");
-    $("#btnProject6>a").attr("href", "https://github.com/AshanSandeep06/My_Chat_Application-INP_Coursework.git");
+    $("#btnProject5>a").attr("href", "https://github.com/Dilshan-Dekumpitiya/Connect-Four-Game-Using-JavaFX");
 });
 
 $('#btnAllProjects').on('click', function () {
